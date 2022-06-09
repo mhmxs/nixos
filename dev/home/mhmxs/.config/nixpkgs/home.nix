14082e206a35dfe5c8f851f74c96791dc2591227
Richard Kovacs <kovacsricsi@gmail.com>
2022-06-09 09:53:38 +0200

    Move things from bashrc to home manager

--- dev/home/mhmxs/.config/nixpkgs/home.nix
+++ dev/home/mhmxs/.config/nixpkgs/home.nix
@@ -5,50 +5,69 @@
   home.homeDirectory = "/home/mhmxs";
 
   home.packages = [
-	pkgs.go_1_17
-	pkgs.nodejs-16_x
-	pkgs.gcc
-	pkgs.gnumake
-	pkgs.git
-    	pkgs.duplicity
-	pkgs.kubectl
-        pkgs.kind
-	pkgs.tunnelto
-        pkgs.k9s
-        pkgs.krew
-	pkgs.kube-capacity
-        pkgs.rake
-        pkgs.gh
-        pkgs.bat
-        pkgs.yq
-        pkgs.jq
-        pkgs.tldr
-        pkgs.lsd
-        pkgs.bat
-        pkgs.fzf
-        pkgs.starship
-        pkgs.ripgrep
-        pkgs.unzip
-        pkgs.diff-so-fancy
-        pkgs.lazydocker
-        pkgs.colordiff
-        pkgs.google-cloud-sdk
-        pkgs.awscli
-        pkgs.aws-iam-authenticator
-        pkgs.eksctl
-        pkgs.azure-cli
+    pkgs.go_1_18
+    pkgs.nodejs-16_x
+    pkgs.gcc
+    pkgs.gnumake
+    pkgs.git
+    pkgs.duplicity
+    pkgs.kubectl
+    pkgs.kind
+    pkgs.tunnelto
+    pkgs.k9s
+    pkgs.krew
+    pkgs.kube-capacity
+    pkgs.rake
+    pkgs.gh
+    pkgs.bat
+    pkgs.yq
+    pkgs.jq
+    pkgs.tldr
+    pkgs.lsd
+    pkgs.bat
+    pkgs.fzf
+    pkgs.starship
+    pkgs.ripgrep
+    pkgs.unzip
+    pkgs.diff-so-fancy
+    pkgs.lazydocker
+    pkgs.colordiff
+    pkgs.google-cloud-sdk
+    pkgs.awscli
+    pkgs.aws-iam-authenticator
+    pkgs.eksctl
+    pkgs.azure-cli
   ];
 
   programs.bash = {
     enable = true;
+    sessionVariables = {
+      EDITOR = "vim";
+      DOCKER_BUILDKIT = "1";
+      GOPATH = "$HOME/.go/1_18";
+    };
+    shellAliases = {
+      ls = "lsd";
+      less = "bat";
+      man = "tldr";
+      diff = "colordiff";
+    };
     bashrcExtra = ''
       . ~/.mybashrc
     '';
   };
 
+  home.sessionPath = [
+    "/usr/local/bin"
+    "$HOME/.go/1_18/bin"
+    "$HOME/.krew/bin"
+  ];
+
   programs.direnv = {
-	enable = true;
-	nix-direnv.enable = true;
-	enableBashIntegration = true;
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv = {
+      enable = true;
+    };
   };
 }
