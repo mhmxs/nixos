e6f5a1c2c17a33f3394bd3ed17260cee601639ce
Richard Kovacs <kovacsricsi@gmail.com>
2023-01-31 20:06:24 +0100

    Cleanup temp directory

--- dev/home/mhmxs/.oh-my-bash/custom/plugins/sync/sync.plugin.sh
+++ dev/home/mhmxs/.oh-my-bash/custom/plugins/sync/sync.plugin.sh
@@ -33,13 +33,15 @@ cleanup() {
     for v in `sudo ignite vm -q`; do sudo ignite rm vm $v; done
     for v in `sudo ignite image -q`; do sudo ignite image rm $v; done
 
-    for v in `ls ~/.go`; do sudo rm -rf .go/$v/pkg/; done
+    for v in `ls ~/.go`; do sudo rm -rf .go/$v/pkg/mod; done
 
     rm -rf $HOME/.kube/cache
     
     sudo rm -rf $HOME/.local/share/kubebuilder-envtest
 
     sudo rm -rf $HOME/src/github.com/kubernetes/kubernetes/_output/local/go/cache
+
+    sudo rm -rf /tmp/*
 
     rm -rf $HOME/.vagrant.d/boxes
     
